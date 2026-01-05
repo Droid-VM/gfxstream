e253c2ac17b23a21eb6937f703fd0a2a90420943
Serdar Kocdemir <kocdemir@google.com>
2026-01-05 16:11:50 +0000

    Fix memory leak on dispatch tables
    Also remove 'ownsDispatch' argument for vulkan object boxing, as
    all the use cases with a dispatch table given won't own it and
    any creation must be owned. We don't need a reference counting to
    delete the table for valid vulkan, but that can be implemented later
    if we start hitting use-after-free cases due to invalid usages.
    
    Bug: 472277856
    Test: manual
    Change-Id: Iaf22e4d0c4846b16ffd88e710392beed06b27344

--- host/vulkan/vk_decoder_global_state.cpp
+++ host/vulkan/vk_decoder_global_state.cpp
@@ -1140,7 +1140,7 @@ class VkDecoderGlobalState::Impl {
                                                              info.applicationName.c_str());
 #endif
         // Box it up
-        VkInstance boxed = new_boxed_VkInstance(*pInstance, nullptr, true /* own dispatch */);
+        VkInstance boxed = new_boxed_VkInstance(*pInstance, nullptr);
         init_vulkan_dispatch_from_instance(m_vk, *pInstance, dispatch_VkInstance(boxed));
         info.boxed = boxed;
 
@@ -1324,8 +1324,7 @@ class VkDecoderGlobalState::Impl {
                 VALIDATE_NEW_HANDLE_INFO_ENTRY(mPhysdevInfo, physicalDevices[i]);
                 auto& physdevInfo = mPhysdevInfo[physicalDevices[i]];
                 physdevInfo.instance = instance;
-                physdevInfo.boxed = new_boxed_VkPhysicalDevice(physicalDevices[i], vk,
-                                                               false /* does not own dispatch */);
+                physdevInfo.boxed = new_boxed_VkPhysicalDevice(physicalDevices[i], vk);
 
                 vk->vkGetPhysicalDeviceProperties(physicalDevices[i], &physdevInfo.props);
 
@@ -2266,7 +2265,7 @@ class VkDecoderGlobalState::Impl {
         }
 
         // First, get the dispatch table.
-        VkDevice boxedDevice = new_boxed_VkDevice(*pDevice, nullptr, true /* own dispatch */);
+        VkDevice boxedDevice = new_boxed_VkDevice(*pDevice, nullptr);
 
         if (mLogging) {
             GFXSTREAM_INFO("%s: init vulkan dispatch from device", __func__);
@@ -2341,8 +2340,7 @@ class VkDecoderGlobalState::Impl {
                 if (mLogging) {
                     GFXSTREAM_INFO("%s: get device queue (end)", __func__);
                 }
-                auto boxedQueue =
-                    new_boxed_VkQueue(physicalQueue, dispatch, false /* does not own dispatch */);
+                auto boxedQueue = new_boxed_VkQueue(physicalQueue, dispatch);
                 extraHandles.push_back((uint64_t)boxedQueue);
 
                 VALIDATE_NEW_HANDLE_INFO_ENTRY(mQueueInfo, physicalQueue);
@@ -2381,8 +2379,7 @@ class VkDecoderGlobalState::Impl {
                         uint64_t virtualQueue64 = (physicalQueue64 | QueueInfo::kVirtualQueueBit);
                         VkQueue virtualQueue = reinterpret_cast<VkQueue>(virtualQueue64);
 
-                        auto boxedVirtualQueue = new_boxed_VkQueue(
-                            virtualQueue, dispatch, false /* does not own dispatch */);
+                        auto boxedVirtualQueue = new_boxed_VkQueue(virtualQueue, dispatch);
                         extraHandles.push_back((uint64_t)boxedVirtualQueue);
 
                         VALIDATE_NEW_HANDLE_INFO_ENTRY(mQueueInfo, virtualQueue);
@@ -6884,8 +6881,7 @@ class VkDecoderGlobalState::Impl {
             mCommandBufferInfo[pCommandBuffers[i]].device = device;
             mCommandBufferInfo[pCommandBuffers[i]].debugUtilsHelper = deviceInfo->debugUtilsHelper;
             mCommandBufferInfo[pCommandBuffers[i]].cmdPool = pAllocateInfo->commandPool;
-            auto boxed = new_boxed_VkCommandBuffer(pCommandBuffers[i], vk,
-                                                   false /* does not own dispatch */);
+            auto boxed = new_boxed_VkCommandBuffer(pCommandBuffers[i], vk);
             mCommandBufferInfo[pCommandBuffers[i]].boxed = boxed;
 
             commandPoolInfo->cmdBuffers.insert(pCommandBuffers[i]);
