441e7b9f6edec15d7e1717039a73cb05e3c7d1b0
Serdar Kocdemir <kocdemir@google.com>
2026-01-13 11:25:17 +0000

    Remove unused variables
    Bug: N/A
    Test: build
    Change-Id: I0cb16f12a455db9780833321c171e8c4c6b89e42

--- host/frame_buffer.cpp
+++ host/frame_buffer.cpp
@@ -851,7 +851,6 @@ class FrameBuffer::Impl : public gfxstream::base::EventNotificationSupport<Frame
     bool m_useSubWindow = false;
 
     bool m_fpsStats = false;
-    bool m_perfStats = false;
     int m_statsNumFrames = 0;
     long long m_statsStartTime = 0;
 
